f08cc903c61fa6d2a6640ee4ac3bb5db497bccba
OzdoevaLeiila <tyuiu.ozdoevalm@outlook.com>
2025-10-15 19:52:47 +0500

    Add project files.

---
+++ Tyuiu.OzdoevaLM.Sprint2.Task0.V15/Program.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.OzdoevaLM.Sprint2.Task0.V15.Lib;
+
+namespace Tyuiu.OzdoevaLM.Sprint2.V15;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.Title = "Спринт #2 | Выполниа: Оздоева Л.М. | ИИПб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("#* Спринт #2                                                              *");
+            Console.WriteLine("* Тема: Операции сравнения                                                *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #15                                                             *");
+            Console.WriteLine("* Выполниа: Оздоева Л.М. | ИИПб-25-1                                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Условие:                                                                *");
+            Console.WriteLine("* Написать программу из операций сравнений (==, !=, <, >, <=, >=,         *");
+            Console.WriteLine("* последовательность операций не должна нарушаться) и                     *");
+            Console.WriteLine("* арифметических выражений, которая вернет логическую последовательность  *");
+            Console.WriteLine("* (массив): (False, False, True, True, False, False), при                 *");
+            Console.WriteLine("* x = 3105, y = 275                                                       *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Исходные данные:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+            int x = 3105, y = 275;
+            bool[] res = new bool[6];
+            res = ds.GetCompareOperations(x, y);
+            Console.WriteLine("X = " + x);
+            Console.WriteLine("Y = " + y);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+            Console.ReadKey();
+    }
+}
