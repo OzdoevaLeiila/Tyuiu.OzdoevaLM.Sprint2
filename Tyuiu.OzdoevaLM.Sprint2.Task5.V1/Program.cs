ac7fc6106e4b68b48b606c2e61feb20bb60b1363
OzdoevaLeiila <tyuiu.ozdoevalm@outlook.com>
2025-10-16 20:34:34 +0500

    Task5

---
+++ Tyuiu.OzdoevaLM.Sprint2.Task5.V1/Program.cs
@@ -0,0 +1,48 @@
+﻿using Tyuiu.OzdoevaLM.Sprint2.Task5.V1.Lib;
+
+namespace Tyuiu.OzdoevaLM.Sprint2.Task5.V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #2 | Выполниа: Оздоева Л.М. | ИИПб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Оператор switch                                                   *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #1                                                              *");
+            Console.WriteLine("* Выполниа: Оздоева Л.М. | ИИПб-25-1                                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Условие:                                                                *");
+            Console.WriteLine("*Написать программу,которая использует оператор switch вычисляет требуемое*");
+            Console.WriteLine("* значение и возвращает результат. По данному номеру месяца, определить   *");
+            Console.WriteLine("* количество дней в этом месяце.                                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Исходные данные:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+            Console.WriteLine("Введите номер месяца: ");
+            int numMouth = Convert.ToInt32(Console.ReadLine());
+
+            string res;
+
+            if ((numMouth < 1) || (numMouth > 12))
+            {
+                res = "Введенно неверное значение!";
+            }
+            else
+            {
+                res = "В этом месяце: " + ds.FindMonthDaysCount(numMouth);
+            }
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(res);
+
+            Console.ReadKey();
+        }
+    }
+}
