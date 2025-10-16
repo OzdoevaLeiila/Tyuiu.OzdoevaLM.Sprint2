00a6ed9c278de2d7193e9e157a877117805cc00e
OzdoevaLeiila <tyuiu.ozdoevalm@outlook.com>
2025-10-16 08:43:03 +0500

    Task1

---
+++ Tyuiu.OzdoevaLM.Sprint2.Task1.V27/Program.cs
@@ -0,0 +1,53 @@
+﻿using Tyuiu.OzdoevaLM.Sprint2.Task1.V27.Lib;
+
+namespace Tyuiu.OzdoevaLM.Sprint2.Task1.V27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Title = "Спринт #2 | Выполниа: Оздоева Л.М. | ИИПб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("#* Спринт #2                                                              *");
+            Console.WriteLine("* Тема: Логические операции                                               *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #27                                                             *");
+            Console.WriteLine("* Выполниа: Оздоева Л.М. | ИИПб-25-1                                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Условие:                                                                *");
+            Console.WriteLine("* Написать программу из операций сравнений (==, !=, <, >, <=, >=,         *");
+            Console.WriteLine("* последовательность можно чередовать, но использовать один               *");
+            Console.WriteLine("* раз в выражении) и логических операций (|, &, ||, &&, !, ^,             *");
+            Console.WriteLine("* арифметических выражений, которая вернет логическую                     *");
+            Console.WriteLine("* последовательность(массив): (False, False, True, False, True, False),   *");
+            Console.WriteLine("* при a = 654, b = 325, c = 154, d = 333                                  *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Исходные данные:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+            int a = 654;
+            int b = 325;
+            int c = 154;
+            int d = 333;
+            bool[] res = new bool[6];
+            res = ds.GetLogicOperations(a, b, c, d);
+
+            Console.WriteLine("A = " + a);
+            Console.WriteLine("B = " + b);
+            Console.WriteLine("C = " + c);
+            Console.WriteLine("D = " + d);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
