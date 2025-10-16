50aedea87bf25a5d29b7971d6d66b4f62a2def4c
OzdoevaLeiila <tyuiu.ozdoevalm@outlook.com>
2025-10-16 14:35:44 +0500

    Task2

---
+++ Tyuiu.OzdoevaLM.Sprint2.Task2.V10.Lib/DataService.cs
@@ -0,0 +1,30 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.OzdoevaLM.Sprint2.Task2.V10.Lib
+{
+    public class DataService : ISprint2Task2V10
+    {
+        public bool CheckDotInShadedArea(int x, int y)
+        {
+            bool res;
+
+            if
+
+              (((x >= 3) && (x <= 5) && (y >= 3) && (y <= 7)) ||    
+              ((x >= 9) && (x <= 12) && (y >= 3) && (y <= 7)) ||     
+              ((x >= 3) && (x <= 12) && (y >= 9) && (y <= 11)) ||    
+              ((x >= 6) && (x <= 10) && (y >= 12) && (y <= 13)) ||   
+              ((x >= 13) && (x <= 14) && (y >= 2) && (y <= 7)) ||    
+              ((x >= 6) && (x <= 14) && (y >= 13) && (y <= 15)))     
+                                                              
+            {
+                res = true;
+            }
+            else
+            {
+                res = false;
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.OzdoevaLM.Sprint2.Task2.V10/Program.cs
@@ -0,0 +1,50 @@
+﻿using Tyuiu.OzdoevaLM.Sprint2.Task2.V10.Lib;
+
+namespace Tyuiu.OzdoevaLM.Sprint2.Task2.V10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #2 | Выполниа: Оздоева Л.М. | ИИПб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                              *");
+            Console.WriteLine("* Тема: Оператор if – полная и короткая форма записи                      *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #10                                                             *");
+            Console.WriteLine("* Выполниа: Оздоева Л.М. | ИИПб-25-1                                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Условие:                                                                *");
+            Console.WriteLine("* Написать программу на, которая запрашивает целые значения с клавиатуры и*");
+            Console.WriteLine("* вычисляет находится ли точка с координатами X,Y в заштрихованной области*");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Исходные данные:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+           
+            Console.WriteLine("Введите значение переменной Х: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение переменной Y: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            DataService ds = new DataService();
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+
+            if (res)
+            {
+                Console.WriteLine("Точка находится в заштрихованной области");
+            }
+            else
+            {
+                Console.WriteLine("Точка не находится в заштрихованной области");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
